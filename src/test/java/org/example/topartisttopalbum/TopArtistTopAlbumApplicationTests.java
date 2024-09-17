f5b2d88a78f32e336f1246d07a83bc997f2ad34e
laurynasmal <36419581+laurynasmal@users.noreply.github.com>
2024-09-17 09:02:10 +0300

    Quick workaround to skip scheduled task in TopArtistTopAlbumApplicationTests.java

--- src/test/java/org/example/topartisttopalbum/TopArtistTopAlbumApplicationTests.java
+++ src/test/java/org/example/topartisttopalbum/TopArtistTopAlbumApplicationTests.java
@@ -1,10 +1,14 @@
 package org.example.topartisttopalbum;
 
+import org.example.topartisttopalbum.scheduling.ItunesApiCallResetTask;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
+@MockBean(ItunesApiCallResetTask.class)
 class TopArtistTopAlbumApplicationTests {
+
 
     @Test
     void contextLoads() {
